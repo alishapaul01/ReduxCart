0c89a31fe16ab95c2f6e568f4d4a741b416679c4
alishapaul01 <136269506+alishapaul01@users.noreply.github.com>
2023-09-02 01:09:46 +0530

    Fetched Data from Backened

--- App.js
+++ App.js
@@ -3,48 +3,28 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector , useDispatch} from 'react-redux';
-import { showCartActions } from './store/showCartReducer';
 import Notification from './components/UI/Notification'
+import { fetchCartData, sendCartData } from './store/cart-actions';
 let isInitial =true;
 const App=()=> {
   const dispatch= useDispatch()
   const showCart= useSelector(state=> state.showCart.isVisible);
   const cart= useSelector(state=> state.cart);
-  const notification=  useSelector(state=> state.showCart.notification)
-  useEffect(()=>{
-    const sendCartData= async()=>{
-      dispatch(showCartActions.showNotification({
-        status: 'pending',
-        title: "Sending...",
-        message: 'Sending Cart Data!'
-      }))
-    const response= await fetch('https://react-https-4e350-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',{
-      method: 'PUT',
-      body: JSON.stringify(cart)
-      }
-    );
-    if(!response.ok){
-      throw new Error('Sending cart data failed')
-    }
-    dispatch(showCartActions.showNotification({
-      status: 'success',
-      title: "Success!",
-      message: 'Sent Cart Data Successfully!'
-    })
-    );
-  }
+  const notification=  useSelector(state=> state.showCart.notification);
 
-  if (isInitial){
-    isInitial= false;
-    return
-  }
-  sendCartData().catch(error=>
-    dispatch(showCartActions.showNotification({
-      status: 'error',
-      title: "Error!",
-      message: 'Sending Cart data failed!'
-    })))
+  useEffect(()=>{
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(()=>{
+    if(isInitial){
+      isInitial= false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));
   }, [cart, dispatch])
+
   return (
     <>
     {notification && (
@@ -53,8 +33,8 @@ const App=()=> {
     title= {notification.title}
     message= {notification.message}/>)}
     <Layout>
-      {showCart && <Cart />}
-      <Products />
+    {showCart && <Cart />}
+    <Products />
     </Layout>
     </>
   );
--- cartReducer.js
+++ cartReducer.js
@@ -6,6 +6,10 @@ const cartSlice= createSlice({
     initialState:{items:[], totalQuantity:0},
     reducers:
     {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+          },
         addtocart(state, action){
             const newItem= action.payload;
             const existingItem= state.items.find(item=> item.id === newItem.id)
